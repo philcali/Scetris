4b2595ab788cc15a3511332fb6d89f25eddc07a2
Philip Cali <philip.cali@gmail.com>
2011-03-18 14:59:01 -0500

    Updated the Scetris game. New lwjgl plugin.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class Plugin(info: ProjectInfo) extends PluginDefinition(info) {
-  val lwjgl = "calico" % "sbt-lwjgl-plugin" % "1.0"
+  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.2"
 }
 
 // vim: set ts=4 sw=4 et:
