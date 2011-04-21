da866ce6dba76b87ba74932ba4d5da4596d350ba
Philip Cali <philip.cali@gmail.com>
2011-04-20 23:38:54 -0500

    Updated plugin info.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class Plugin(info: ProjectInfo) extends PluginDefinition(info) {
-  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.2"
+  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.5"
 }
 
 // vim: set ts=4 sw=4 et:
